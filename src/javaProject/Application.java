1d5ed29e7ac21af158fbe0c734e582c3bf82177a
KensalRamos <kensalramos@gmail.com>
2018-12-05 12:28:13 -0500

    Update Application.java

--- src/javaProject/Application.java
+++ src/javaProject/Application.java
@@ -1,3 +1,22 @@
+package javaProject;
+
 /*
-* Main Java File
-*/
+ * File Name: 		Application.java
+ * Name:			Kensal Ramos, Carlos, Jaime 
+ * Course:			COP 2800C at Valencia College
+ * Instructor:		David Stendel
+ * Description:		Final project for java course...
+ * 
+ * Team members:	Kensal J. Ramos, Jaime, Carlos
+ * Date:			5 December 2018	-	Main class created
+ * 
+ */
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
