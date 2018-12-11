565271d4f14d83ebcfe72fcf646e8759d0e87846
KensalRamos <kensalramos@gmail.com>
2018-12-11 15:09:33 -0500

    Create Client.java

---
+++ src/javaProject/Client.java
@@ -0,0 +1,21 @@
+package javaProject;
+
+/*
+ * File Name: 		Application.java
+ * Name:			Kensal Ramos, Carlos, Jaime 
+ * Course:			COP 2800C at Valencia College
+ * Instructor:		David Stendel
+ * Description:		Client class holds the basic information that all clients need.
+ */
+
+public class Client extends Person {
+
+	// Fields
+	
+	public Client() {
+		
+	}
+	
+	
+	
+}
