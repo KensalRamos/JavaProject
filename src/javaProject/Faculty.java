a113a1ad6dce845a47bc3ea782db1f5c283efe80
KensalRamos <kensalramos@gmail.com>
2018-12-11 15:10:04 -0500

    Create Faculty.java

---
+++ src/javaProject/Faculty.java
@@ -0,0 +1,17 @@
+package javaProject;
+
+/*
+ * File Name: 		Faculty.java
+ * Name:			    Kensal Ramos, Carlos, Jaime 
+ * Course:			  COP 2800C at Valencia College
+ * Instructor:		David Stendel
+ * Description:		Faculty class holds the basic information all faculty needs.
+ */
+
+public class Faculty extends Employee {
+
+	public Faculty() {
+		
+	}
+	
+}
